5f64c114ffb6ab8ca7e1fb3c0d6df4fd07fca10e
Roman Weich <roman.weich@gmail.com>
2021-05-27 20:10:06 +0200

    feat: add TitleParametersDidChangeEvent

--- test/Events/Received/Plugin/TitleParametersDidChangeEventTest.ts
+++ test/Events/Received/Plugin/TitleParametersDidChangeEventTest.ts
@@ -7,19 +7,30 @@ import eventMissingParameter from '../fixtures/titleParametersDidChangeEvent.mis
 import eventValid from '../fixtures/titleParametersDidChangeEvent.valid.json';
 import { expect } from 'chai';
 
-describe.skip('TitleParametersDidChangeEvent test', () => {
+describe('TitleParametersDidChangeEvent test', () => {
   it('should create the event when using the correct payload', function () {
     const event = new TitleParametersDidChangeEvent(eventValid);
     expect(event.action).to.equal('tpdc.action');
     expect(event.context).to.equal('conconcon');
     expect(event.device).to.equal('devved');
     expect(event.event).to.equal('titleParametersDidChange');
-    // TODO: add missing tests
+    expect(event.column).to.equal(1);
+    expect(event.row).to.equal(4);
+    expect(Object.keys(event.settings as Record<string, unknown>)).to.be.length(0);
+    expect(event.state).to.equal(0);
+    expect(event.title).to.equal('a title');
+    expect(event.fontFamily).to.equal('font');
+    expect(event.fontSize).to.equal(13);
+    expect(event.fontStyle).to.equal('bold');
+    expect(event.fontUnderline).to.equal(false);
+    expect(event.showTitle).to.equal(true);
+    expect(event.titleAlignment).to.equal('middle');
+    expect(event.titleColor).to.equal('#ffffff');
   });
   it('should throw a validation error on missing parameters', function () {
     expect(() => new TitleParametersDidChangeEvent(eventMissingParameter)).to.throw(
       EventValidationError,
-      /required property 'context'/,
+      /required property 'fontSize'/,
     );
   });
   it('should throw a validation error on wrong event type', function () {
