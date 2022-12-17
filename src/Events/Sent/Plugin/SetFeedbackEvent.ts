572029f4d848ab05edef78a1c93acfc54d1745e0
Kaz Wolfe <root@kazwolfe.io>
2022-12-17 14:06:02 -0500

    docs: Add some minor documentation for new events
    - Add a high level overview for setFeedback and setFeedbackLayout.
    - Add links from the payloa classes back to the actual event classes.

--- src/Events/Sent/Plugin/SetFeedbackEvent.ts
+++ src/Events/Sent/Plugin/SetFeedbackEvent.ts
@@ -1,6 +1,9 @@
 import AbstractSentSetterEvent from '@/Events/Sent/AbstractSentSetterEvent';
 import { GenericLayoutFeedback, LayoutFeedback } from '@/StreamdeckTypes/Received/Feedback/LayoutFeedback';
 
+/**
+ * See the documentation in {@link EventsSent#setFeedback} for information about this class.
+ */
 export default class SetFeedbackEvent extends AbstractSentSetterEvent {
   public readonly event = 'setFeedback';
   public readonly payload: GenericLayoutFeedback;
--- src/Events/Sent/Plugin/SetFeedbackLayoutEvent.ts
+++ src/Events/Sent/Plugin/SetFeedbackLayoutEvent.ts
@@ -1,6 +1,9 @@
 import AbstractSentSetterEvent from '@/Events/Sent/AbstractSentSetterEvent';
 import { LayoutFeedbackKey } from '@/StreamdeckTypes/Received/Feedback/LayoutFeedback';
 
+/**
+ * See the documentation in {@link EventsSent#setFeedbackLayout} for information about this class.
+ */
 export default class SetFeedbackLayoutEvent extends AbstractSentSetterEvent {
   public readonly event = 'setFeedbackLayout';
   public readonly layout: string;
