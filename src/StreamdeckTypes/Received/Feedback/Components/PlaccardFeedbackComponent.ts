335eff19e8463b372f2aca4bfb8f91c611f30052
Kaz Wolfe <root@kazwolfe.io>
2023-06-27 20:54:06 -0700

    chore: Mark placcard component as deprecated
    Elgato guidance is to use the `pixmap` component without a set image for placcard's functionality.

--- src/StreamdeckTypes/Received/Feedback/Components/PlaccardFeedbackComponent.ts
+++ src/StreamdeckTypes/Received/Feedback/Components/PlaccardFeedbackComponent.ts
@@ -2,6 +2,10 @@ import { Static, Type } from '@sinclair/typebox';
 
 import { BaseFeedbackComponentProperties } from './BaseFeedbackComponent';
 
+/**
+ * @deprecated The Placcard feedback component has been marked as deprecated in SDK 6.1 and will be removed in a future
+ * version of the SDK. Users seeking its functionality should use the `pixmap` component without setting an image.
+ */
 export const PlaccardFeedbackComponentProperties = {
   ...BaseFeedbackComponentProperties,
 
@@ -14,5 +18,15 @@ export const PlaccardFeedbackComponentProperties = {
     ]),
   ),
 };
+
+/**
+ * @deprecated The Placcard feedback component has been marked as deprecated in SDK 6.1 and will be removed in a future
+ * version of the SDK. Users seeking its functionality should use the `pixmap` component without setting an image.
+ */
 export const PlaccardFeedbackComponent = Type.Object(PlaccardFeedbackComponentProperties);
+
+/**
+ * @deprecated The Placcard feedback component has been marked as deprecated in SDK 6.1 and will be removed in a future
+ * version of the SDK. Users seeking its functionality should use the `pixmap` component without setting an image.
+ */
 export type PlaccardFeedbackComponent = Static<typeof PlaccardFeedbackComponent>;
